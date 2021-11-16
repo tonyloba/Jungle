aa73a741b6eac985f6bf2d67907f80e331d2736e
tonyloba <tonyloba@gmail.com>
2021-11-16 04:30:34 +0000

    updated some logic for user auth and categories

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # if user = User.authenticate_with_credentials(params[:email], params[:password])
-      user = User.find_by_email(params[:email])
-      if user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
